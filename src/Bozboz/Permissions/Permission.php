485f733fdd16f67206132fa5900684efdc7cb3f7
Sam King <sdking02@gmail.com>
2015-10-22 17:30:56 +0100

    Add validity checks on Permission class

--- src/Bozboz/Permissions/Permission.php
+++ src/Bozboz/Permissions/Permission.php
@@ -6,9 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
+	const WILDCARD = '*';
+
 	protected $fillable = [
 		'action',
-		'param'
+		'param',
+		'user_id',
 	];
 
 	public function label()
@@ -20,5 +23,27 @@ class Permission extends Model
 		}
 
 		return $label;
+	}
+
+	/**
+	 * Determine if current rule matches requested action and parameter
+	 *
+	 * @param  string  $action
+	 * @param  int  $param
+	 * @return boolean
+	 */
+	public function isValid($action, $param)
+	{
+		return $this->isWildCard() || $action === $this->action && $param === $this->param;
+	}
+
+	/**
+	 * Determine if current permission is a wildcard (can do everything)
+	 *
+	 * @return boolean
+	 */
+	protected function isWildCard()
+	{
+		return $this->action === static::WILDCARD;
 	}
 }
