10a1415547ccbf2f41665130eb6178f88bc678bf
Sam King <sdking02@gmail.com>
2015-12-04 14:06:15 +0000

    Refactor GlobalRule

--- src/Bozboz/Permissions/Rules/GlobalRule.php
+++ src/Bozboz/Permissions/Rules/GlobalRule.php
@@ -3,28 +3,10 @@
 namespace Bozboz\Permissions\Rules;
 
 use Bozboz\Permissions\Exceptions\InvalidParameterException;
-use Bozboz\Permissions\UserInterface;
 
 class GlobalRule extends Rule
 {
-	/**
-	 * @inheritdoc
-	 */
-	public function validFor(UserInterface $user, $param)
-	{
-		$this->validate($param);
-
-		return parent::validFor($user, $param);
-	}
-
-	/**
-	 * Determine if the rule can accept a parameter
-	 *
-	 * @param  int  $param
-	 * @throws Bozboz\Permissions\Exceptions\InvalidParameterException
-	 * @return void
-	 */
-	protected function validate($param)
+	protected function getParamForPermission($param)
 	{
 		if ( ! empty($param)) throw new InvalidParameterException(
 			"'{$this->alias}' permission does not accept a parameter"
