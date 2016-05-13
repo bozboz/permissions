efbaeced7c360ce4896b13556bf150d7e2fc3bca
Sam King <sdking02@gmail.com>
2016-05-13 10:31:16 +0100

    Use EloquentPermissionsTrait on ExampleUser

--- src/ExampleUser.php
+++ src/ExampleUser.php
@@ -6,19 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class ExampleUser extends Model implements UserInterface
 {
+	use EloquentPermissionsTrait;
+
 	protected $table = 'users';
 	protected $fillable = [
 		'first_name',
 		'last_name'
 	];
-
-	public function permissions()
-	{
-		return $this->hasMany(Permission::class, 'user_id');
-	}
-
-	public function getPermissions($action, $param = null)
-	{
-		$this->permissions;
-	}
 }
